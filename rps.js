0ff5ea6e1252b68c2d42a0de75db1f25c940e46d
KarimElsayad247 <KarimElsayad247@users.noreply.github.com>
2021-03-23 16:16:10 +0200

    finish project

--- rps.js
+++ rps.js
@@ -14,15 +14,55 @@ function play(playerSelection, computerSelection) {
         playerSelection === "paper" && computerSelection === "rock"    ||
         playerSelection === "scissors" && computerSelection === "paper") {
 
-        return `You Win! ${playerSelection} beats ${computerSelection}!`;;
+        return 1;
     }
     // if tie
     else if (playerSelection === computerSelection) {
-        return "It's a tie!";
+        return 0;
     }
 
     // otherwise it's computer win
     else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+        return -1;
+    }
+}
+
+function game() {
+    
+    playerScore = 0;
+    computerScore = 0;
+    
+    for (i = 0; i < 5; ++i) {
+        playerSelection = prompt("Your selection");
+        computerSelection = computerPlay();
+        result = play(playerSelection, computerSelection);
+        
+        switch (result) {
+            case 1:
+                console.log("You win this round!");
+                playerScore++;
+                break;
+            case 0:
+                console.log("It's a tie!");
+                break;
+            case -1:
+                console.log("Computer wins this round");
+                computerScore++;
+                break;
+        }
+    }
+
+    console.log("Game Over!")
+    console.log(`Player score: ${playerScore}`)
+    console.log(`Computer Score: ${computerScore}`)
+    
+    if (playerScore > computerScore) {
+        console.log("Player wins the game!")
+    }
+    else if (playerScore < computerScore) {
+        console.log("Computer wins the game")
+    }
+    else {
+        console.log("The game is a tie!")
     }
 }
